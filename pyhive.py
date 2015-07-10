0b8de6ffdfdd3c743f0c358965c5797d053772f6
Chris Collins <collins.christopher@gmail.com>
2015-07-10 16:06:27 -0400

    first thoughts

---
+++ pyhive.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# First pass at a single Pymagotchi.  CREATING LIFE! (-ish)
+#
+# Maybe stripped-down Maslo's hierarchy of needs?
+#
+# Physiological:
+#  - food
+#  - cleanliness
+# Safety:
+#  - ?
+# Love/Belonging
+#  - play
+#  - treats
+# Esteem
+#  - ?
+# Self-actualization
+#  - I think perahps ... not
+
+
+# How do I even do this?  Things change over time.  Count real time?
+
+# Do I instantiate a class for each Pymagotchi?
+
+# Wha is the master process?  Are there two?  To be true to the spirit of the
+# comic, I'd need a process to create Pymagotchi, and a separate, unlreated
+# one, to tend to their needs.  Otherwise it defeats the purpose.  The two need
+# to be independent of one another, and the second needs to watch the
+# Pymagotchi created by the first and handle their needs.
+
+# Where to start?
+
+# Oh, and lets not skip past the obvious here.  It's a hive, right?  IT NEEDS A
+# QUEEN!
